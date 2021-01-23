419a62b5754b6fd2be49a292801cd389b3cef38b
Luciano Felix <22255332+FelixLuciano@users.noreply.github.com>
2021-01-23 04:53:45 -0300

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,18 +1,14 @@
 "use strict"
 
 export default async function importFromGist (gistURL) {
-  let text
-  
   try {
-    text = await fetch(gistURL).then(res => res.text())
-  }
-  catch (e) {
-    console.error(e)
-  }
-  finally {
+    const text = await fetch(gistURL).then(res => res.text())
     let blob = new Blob([ text ], { type: "application/javascript" })
     let url  = globalThis.URL.createObjectURL(blob)
 
     return await import(url)
+  }
+  catch (e) {
+    console.error(e)
   }
 }
